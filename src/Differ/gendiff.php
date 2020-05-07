8f82d894a641d468a0ec57f3a6290a0306288db3
Gleb <gleb__l@mail.ru>
2020-05-07 19:59:55 +0300

    finish step 6

--- src/Differ/gendiff.php
+++ src/Differ/gendiff.php
@@ -3,70 +3,137 @@
 namespace Differ\Differ;
 
 use function Funct\Collection\union;
+use function Funct\Collection\flatten;
+use function Funct\Collection\flattenAll;
+use function Funct\Collection\zip;
 
 function genDiff($pathToFile1, $pathToFile2)
 {
     $data1 = parseData($pathToFile1);
     $data2 = parseData($pathToFile2);
+    $data1 = getAst($data1);
+    $data2 = getAst($data2);
+    $astWithStates = getStates($data1, $data2);
+    $renderedData = diffRenderer($astWithStates);
+    $res = "{\n" . stringify($renderedData) . "\n}";
+    return $res;
+}
 
-    $data1 = get_object_vars($data1);
-    $data2 = get_object_vars($data2);
+function getAst($data)
+{
+    $data = get_object_vars($data);
+    $keys = array_keys($data);
+    $a = array_combine($keys, array_map(function ($key) use ($data) {
+        if (is_object($data[$key])) {
+            return ['children' => getAst($data[$key])];
+        }
+        return $data[$key];
+    }, $keys));
 
-    $keysWithStates = getStates($data1, $data2);
+    return $a;
+}
 
-    $diff = array_map(function ($item) {
+
+function diffRenderer($tree)
+{
+    $render = array_map(function ($item) {
+        $type = $item['type'] ?? null;
+        $state = $item['state'] ?? null;
         $key = $item['key'];
-        $value = $item['value'];
-        $value = !is_bool($value) ? $value : boolToString($value);
-        $state = $item['state'];
+        if ($type && !$state) {
+            return [$item['key'] => diffRenderer($item['children'])];
+        }
+        
+        if (isset($item['children'])) {
+            $value = "{{$item['children']['key']}: {$item['children']['value']}}";
+        } else {
+            $value = $item['value'];
+        }
+        $value = is_bool($value) ? boolToString($value) : $value;
         switch ($state) {
             case 'unchanged':
-                return "{$key}: {$value}";
-                break;
+                return "  {$key}: {$value}";
             case 'changed':
-                return "+ {$key}: {$value['before']}\n  - {$key}: {$value['after']}";
-                break;
+                return "+ {$key}: {$value['after']}\n- {$key}: {$value['before']}";
             case 'deleted':
                 return "- {$key}: {$value}";
-                break;
             case 'added':
                 return "+ {$key}: {$value}";
         }
-    }, $keysWithStates);
-    $diff = implode("\n  ", $diff);
-    $diff = "{\n    {$diff}\n}";
-    return $diff;
+    }, $tree);
+
+    return $render;
 }
+
 
 function getStates($data1, $data2)
 {
     $keys1 = array_keys($data1);
     $keys2 = array_keys($data2);
     $unionKeys = array_values(union($keys1, $keys2));
+    sort($unionKeys);
     $statesOfKeys = array_map(function ($key) use ($data1, $data2) {
-        if (isUnchadged($data1, $data2, $key)) {
+        $children1 = $data1[$key]['children'] ?? false;
+        $children2 = $data2[$key]['children'] ?? false;
+        if ($children1 && $children2) {
             return ['key' => $key,
-                    'value' => $data1[$key],
+                    'children' => getStates($children1, $children2),
+                    'type' => 'nested'];
+        }
+        
+        if ($children1 || $children2) {
+            $value = 'children';
+            $type = ['type' => 'nested'];
+        } else {
+            $value = 'value';
+        }
+
+        
+        if (isUnchadged($data1, $data2, $key)) {
+            $res = ['key' => $key,
+                    $value => $data1[$key],
                     'state' => 'unchanged'];
+            return isset($type) ? array_merge($res, $type) : $res;
         }
         if (isChanged($data1, $data2, $key)) {
-            return ['key' => $key,
-                    'value' => ['before' => $data1[$key], 'after' => $data2[$key]],
+            $res = ['key' => $key,
+                    $value => ['before' => $data1[$key], 'after' => $data2[$key]],
                     'state' => 'changed'];
+                    return isset($type) ? array_merge($res, $type) : $res;
         }
         if (isDeleted($data1, $data2, $key)) {
-            return ['key' => $key,
-                    'value' => $data1[$key],
+            $res = ['key' => $key,
+                    $value => $children1 ? ['key' => key($children1), 'value' => $children1[key($children1)]]
+                                        : $data1[$key],
                     'state' => 'deleted'];
+                    return isset($type) ? array_merge($res, $type) : $res;
         }
         if (isAdded($data1, $data2, $key)) {
-            return ['key' => $key,
-                    'value' => $data2[$key],
+            $res = ['key' => $key,
+                    $value => $children2 ? ['key' => key($children2), 'value' => $children2[key($children2)]]
+                                        : $data2[$key],
                     'state' => 'added'];
+                    return isset($type) ? array_merge($res, $type) : $res;
         }
     }, $unionKeys);
 
     return $statesOfKeys;
+}
+
+function stringify($diff, $spaces = '  ')
+{
+    $diff = array_map(function ($item) use ($spaces) {
+        if (is_array($item)) {
+            $key = key($item);
+            return "  {$spaces}{$key}: {\n" . stringify($item[$key], $spaces = "{$spaces}    ") . "\n    }";
+        }
+        $item = str_replace("\n-", "\n{$spaces}-", $item);
+        $item = str_replace("{", "{\n{$spaces}      ", $item);
+        $item = str_replace("}", "\n{$spaces}  }", $item);
+        return "{$spaces}{$item}";
+    }, $diff);
+    $res = implode("\n", $diff);
+    return $res;
 }
 
 function isUnchadged($data1, $data2, $key)
@@ -100,6 +167,7 @@ function isAdded($data1, $data2, $key)
         return true;
     }
 }
+
 
 function boolToString($string)
 {
--- tests/DiffTest.php
+++ tests/DiffTest.php
@@ -17,33 +17,57 @@ class DifferTest extends TestCase
         $this->path = __DIR__. '/fixtures';
     }
 
-    public function testLoadJson()
+    public function testPlainFiles()
     {
-        $diff = genDiff("{$this->path}/before.json", "{$this->path}/after.json");
+        $json = genDiff("{$this->path}/before.json", "{$this->path}/after.json");
+        $yaml = genDiff("{$this->path}/first.yaml", "{$this->path}/second.yaml");
         $expected = <<<DOC
 {
     host: hexlet.io
-  + timeout: 50
-  - timeout: 20
   - proxy: 123.234.53.22
+  + timeout: 20
+  - timeout: 50
   + verbose: true
 }
 DOC;
-        $this->assertEquals($expected, $diff);
+        $this->assertEquals($expected, $json);
+        $this->assertEquals($expected, $yaml);
     }
 
-    public function testLoadYaml()
+
+
+    public function testRecursiveFiles()
     {
-        $diff = genDiff("{$this->path}/first.yaml", "{$this->path}/second.yaml");
+        $json = genDiff("{$this->path}/beforeRecursive.json", "{$this->path}/afterRecursive.json");
+        $yaml = genDiff("{$this->path}/firstRecursive.yaml", "{$this->path}/secondRecursive.yaml");
         $expected = <<<DOC
 {
-    host: hexlet.io
-  + timeout: 50
-  - timeout: 20
-  - proxy: 123.234.53.22
-  + verbose: true
+    common: {
+        setting1: Value 1
+      - setting2: 200
+        setting3: true
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+      - setting6: {
+            key: value
+        }
+    }
+    group1: {
+      + baz: bars
+      - baz: bas
+        foo: bar
+    }
+  - group2: {
+        abc: 12345
+    }
+  + group3: {
+        fee: 100500
+    }
 }
 DOC;
-        $this->assertEquals($expected, $diff);
+        $this->assertEquals($expected, $json);
+        $this->assertEquals($expected, $yaml);
     }
 }
