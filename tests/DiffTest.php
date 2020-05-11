ae6d30b79448c5c93ea894cd666ad6287128488c
Gleb <gleb__l@mail.ru>
2020-05-11 14:56:03 +0300

    fix tests

--- tests/DiffTest.php
+++ tests/DiffTest.php
@@ -4,8 +4,6 @@ namespace Differ\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-use Differ\Differ;
-
 use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
@@ -14,77 +12,24 @@ class DifferTest extends TestCase
 
     public function setUp(): void
     {
-        $this->path = __DIR__. '/fixtures';
+        $this->path = __DIR__ . '/fixtures/';
     }
 
-    public function testPlainFiles()
+    public function testPrettyFormatter()
     {
-        $json = genDiff("{$this->path}/before.json", "{$this->path}/after.json");
-        $yaml = genDiff("{$this->path}/first.yaml", "{$this->path}/second.yaml");
-        $expected = <<<DOC
-{
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  + timeout: 20
-  - timeout: 50
-  + verbose: true
-}
-DOC;
-        $this->assertEquals($expected, $json);
-        $this->assertEquals($expected, $yaml);
-    }
-
-
-
-    public function testRecursiveFiles()
-    {
-        $json = genDiff("{$this->path}/beforeRecursive.json", "{$this->path}/afterRecursive.json");
-        $yaml = genDiff("{$this->path}/firstRecursive.yaml", "{$this->path}/secondRecursive.yaml");
-        $expected = <<<DOC
-{
-    common: {
-        setting1: Value 1
-      - setting2: 200
-        setting3: true
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-      - setting6: {
-            key: value
-        }
-    }
-    group1: {
-      + baz: bars
-      - baz: bas
-        foo: bar
-    }
-  - group2: {
-        abc: 12345
-    }
-  + group3: {
-        fee: 100500
-    }
-}
-DOC;
-        $this->assertEquals($expected, $json);
-        $this->assertEquals($expected, $yaml);
+        $jsonDiff = genDiff("{$this->path}before.json", "{$this->path}after.json");
+        $yamlDiff = genDiff("{$this->path}first.yaml", "{$this->path}second.yaml");
+        $expected = file_get_contents("{$this->path}resultPretty.txt");
+        $this->assertEquals($expected, $jsonDiff);
+        $this->assertEquals($expected, $yamlDiff);
     }
 
     public function testPlainFormatter()
     {
-        $json = genDiff("{$this->path}/beforeRecursive.json", "{$this->path}/afterRecursive.json", 'plain');
-        $yaml = genDiff("{$this->path}/firstRecursive.yaml", "{$this->path}/secondRecursive.yaml", 'plain');
-        $expected = <<<DOC
-Property 'common.setting2' was removed
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: 'complex value'
-Property 'common.setting6' was removed
-Property 'group1.baz' was changed. From 'bas' to 'bars'
-Property 'group2' was removed
-Property 'group3' was added with value: 'complex value'
-DOC;
-        $this->assertEquals($expected, $json);
-        $this->assertEquals($expected, $yaml);
+        $jsonDiff = genDiff("{$this->path}before.json", "{$this->path}after.json", 'plain');
+        $yamlDiff = genDiff("{$this->path}first.yaml", "{$this->path}second.yaml", 'plain');
+        $expected = file_get_contents("{$this->path}resultPlain.txt");
+        $this->assertEquals($expected, $jsonDiff);
+        $this->assertEquals($expected, $yamlDiff);
     }
 }
