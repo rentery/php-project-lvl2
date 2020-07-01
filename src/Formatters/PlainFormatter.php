27a9815d35360ea6044578091ad1e94e0d0d5f20
Gleb <gleb__l@mail.ru>
2020-07-01 17:46:57 +0300

    fix formatters

--- src/Formatters/PlainFormatter.php
+++ src/Formatters/PlainFormatter.php
@@ -13,31 +13,34 @@ function plainFormatter($configTree)
     return $diff;
 }
 
-function plainRenderer($tree, $rootKey = '')
+function plainRenderer($tree)
 {
-    $render = array_map(function ($node) use ($rootKey) {
-        $type = $node['type'];
-        $key = $node['key'];
-        
-        switch ($type) {
-            case 'node':
-                return [plainRenderer($node['children'], "{$rootKey}{$node['key']}.")];
-            case 'unchanged':
-                return null;
-            case 'changed':
-                $oldValue = plainStringify($node['oldValue']);
-                $newValue = plainStringify($node['newValue']);
-                return "Property '{$rootKey}{$key}' was changed. From '{$oldValue}' to '{$newValue}'";
-            case 'deleted':
-                return "Property '{$rootKey}{$key}' was removed";
-            case 'added':
-                $value = plainStringify($node['value']);
-                return "Property '{$rootKey}{$key}' was added with value: '{$value}'";
-            default:
-                throw new \Exception("Unknown type: {$type}");
-        }
-    }, $tree);
-    return $render;
+    $iter = function ($tree, $rootKey = '') use (&$iter) {
+        $renderedData = array_map(function ($node) use (&$iter, $rootKey) {
+            $type = $node['type'];
+            $key = $node['key'];
+
+            switch ($type) {
+                case 'node':
+                    return [$iter($node['children'], "{$rootKey}{$node['key']}.")];
+                case 'unchanged':
+                    return null;
+                case 'changed':
+                    $oldValue = plainStringify($node['oldValue']);
+                    $newValue = plainStringify($node['newValue']);
+                    return "Property '{$rootKey}{$key}' was changed. From '{$oldValue}' to '{$newValue}'";
+                case 'deleted':
+                    return "Property '{$rootKey}{$key}' was removed";
+                case 'added':
+                    $value = plainStringify($node['value']);
+                    return "Property '{$rootKey}{$key}' was added with value: '{$value}'";
+                default:
+                    throw new \Exception("Unknown type: {$type}");
+            }
+        }, $tree);
+        return $renderedData;
+    };
+    return $iter($tree);
 }
 
 
--- src/Formatters/PrettyFormatter.php
+++ src/Formatters/PrettyFormatter.php
@@ -2,46 +2,58 @@
 
 namespace Differ\Formatters;
 
+const COUNT_OF_SPACES_UNCHANGED_CASE = 4;
+const COUNT_OF_SPACES_OTHER_CASES = 2;
+
 function prettyFormatter($configTree)
 {
     $renderedConfigTree = prettyRenderer($configTree);
     return "{\n" . $renderedConfigTree . "\n}";
 }
 
-function prettyRenderer($tree, $indent = '  ')
+function prettyRenderer($tree)
 {
-    $renderedData = array_map(function ($node) use ($indent) {
-        $type = $node['type'];
-        $key = $node['key'];
 
-        switch ($type) {
-            case 'node':
-                return "    {$node['key']}: {\n" . prettyRenderer($node['children'], "{$indent}    ") . "\n    }";
-            case 'unchanged':
-                $value = prettyStringify($node['value'], $indent);
-                return "  {$indent}{$key}: {$value}";
-            case 'changed':
-                $oldValue = prettyStringify($node['oldValue']);
-                $newValue = prettyStringify($node['newValue']);
-                return "{$indent}+ {$key}: {$newValue}\n{$indent}- {$key}: {$oldValue}";
-            case 'deleted':
-                $value = prettyStringify($node['value'], $indent);
-                return "{$indent}- {$key}: {$value}";
-            case 'added':
-                $value = prettyStringify($node['value'], $indent);
-                return "{$indent}+ {$key}: {$value}";
-            default:
-                throw new \Exception('Unknown type: {$type}');
-        }
-    }, $tree);
+    $iter = function ($tree, $depth = 1) use (&$iter) {
+        $renderedData = array_map(function ($node) use ($iter, $depth) {
+            $type = $node['type'];
+            $key = $node['key'];
+            $indent = str_repeat(' ', $depth * COUNT_OF_SPACES_UNCHANGED_CASE);
+            $indentSmall = str_repeat(' ', $depth * COUNT_OF_SPACES_UNCHANGED_CASE - COUNT_OF_SPACES_OTHER_CASES);
 
-    return is_array($renderedData) ? implode("\n", $renderedData) : $renderedData;
+            switch ($type) {
+                case 'node':
+                    return "{$indent}{$node['key']}: {\n" . $iter($node['children'], $depth + 1) . "\n    }";
+                case 'unchanged':
+                    $value = prettyStringify($node['value'], $indent);
+                    return "{$indent}{$key}: {$value}";
+                case 'changed':
+                    $oldValue = prettyStringify($node['oldValue']);
+                    $newValue = prettyStringify($node['newValue']);
+                    return "{$indentSmall}+ {$key}: {$newValue}\n{$indentSmall}- {$key}: {$oldValue}";
+                case 'deleted':
+                    $value = prettyStringify($node['value'], $indent);
+                    return "{$indentSmall}- {$key}: {$value}";
+                case 'added':
+                    $value = prettyStringify($node['value'], $indent);
+                    return "{$indentSmall}+ {$key}: {$value}";
+                default:
+                    throw new \Exception('Unknown type: {$type}');
+            }
+        }, $tree);
+    
+        return is_array($renderedData) ? implode("\n", $renderedData) : $renderedData;
+    };
+
+    $renderedData = $iter($tree);
+    return $renderedData;
 }
 
 function prettyStringify($item, $indent = '')
 {
     $value = json_encode($item);
+    $indentLeft = str_repeat(' ', strlen($indent) + COUNT_OF_SPACES_UNCHANGED_CASE);
     $search = ['{', '}', ':', '"'];
-    $replace = ["{\n      {$indent}", "\n{$indent}  }", ': ', ''];
+    $replace = ["{\n{$indentLeft}", "\n{$indent}}", ': ', ''];
     return str_replace($search, $replace, $value);
 }
